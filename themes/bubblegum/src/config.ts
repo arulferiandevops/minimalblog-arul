6000a80362fdee84b8418134b68cbce2d2cedb65
arulferiandevops <149508752+arulferiandevops@users.noreply.github.com>
2023-11-28 13:49:07 +0700

    Update config.ts

--- themes/bubblegum/src/config.ts
+++ themes/bubblegum/src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "arulferianr";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Sharing tentang SRE/DevOps";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Arul Ferian Ramadloni";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
